1b403cde7bb06c251fc3e2ac6c881805f468fd54
nkrapp <nico.krapp@suse.com>
2025-12-02 13:01:58 +0100

    Solution for Day 02

---
+++ day02/src/bin/day02-1.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+fn main() {
+    let path = "day02-input.txt";
+    let error_msg = "Should be able to read input file";
+    let input = fs::read_to_string(path).expect(error_msg);
+    let result = process(input);
+    println!("{}", result);
+}
+
+pub fn process(input: String) -> u64 {
+    let mut results: Vec<u64> = Vec::new();
+    let ranges: Vec<(u64, u64)> = input
+        .split(",")
+        .map(|x| x.split_once("-").unwrap())
+        .map(|tup| {
+            let s = tup.0.parse::<u64>().unwrap();
+            let e = tup.1.parse::<u64>().unwrap();
+            return (s, e);
+        })
+        .collect();
+    for range in ranges {
+        for num in range.0..range.1 + 1 {
+            let tmp = num.to_string();
+            let mid = tmp.len() / 2;
+            let (first, second) = tmp.split_at(mid);
+            if first == second {
+                results.push(num);
+            }
+        }
+    }
+
+    return results.iter().sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn day02_part1_test() {
+        let path = "day02-test.txt";
+        let test_input = fs::read_to_string(path).expect("Should be able to read input");
+        let result = process(test_input);
+        assert_eq!(result, 1227775554);
+    }
+}
